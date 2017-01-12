95e28c0d35a1210fe987506ba7338fba3122dcfa
Joseph McBride <jmcbriddle@gmail.com>
2017-01-11 23:03:38 -0800

    updated mapPoint class

--- mapPoint.js
+++ mapPoint.js
@@ -1,6 +1,24 @@
 export default class mapPoint {
+
   constructor(x, y) {
     this.x = x;
+    this.y = y;
+  }
+
+
+  getX() {
+    return this.x;
+  }
+
+  getY() {
+    return this.y;
+  }
+
+  setX(x) {
+    this.x = x;
+  }
+
+  setY(y) {
     this.y = y;
   }
 }
